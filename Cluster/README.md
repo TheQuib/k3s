e275633d93ff8c5d1585b525e90e2c91fc605fe2
Quinn Henry <78760148+Brit-Boi@users.noreply.github.com>
2022-02-10 21:51:55 -0500

    Update cluster docs

--- Cluster/README.md
+++ Cluster/README.md
@@ -4,6 +4,10 @@ The bash shell scripts in this directory can be run to create and add nodes/agen
 These are relatively self documented.
 
 ## Order of operations
-1. k3s-node-setup.sh
-1. k3s-agent-setup.sh
-1. k3s-node-helm-setup.sh
+1. [k3s-server-setup.sh](/k3s-server-setup.sh)
+1. [k3s-agent-setup.sh](/k3s-agent-setup.sh)
+1. [k3s-server-helm-setup.sh](/k3s-server-helm-setup.sh)
+
+## Useful commands
+Get Kubernetes token (from master server):
+  - `sudo cat /var/lib/rancher/k3s/server/node-token`
---
+++ Cluster/k3s-server-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Set of commands to be used for initial setup of HA Kubernetes cluster node servers (masters)
+
+# Variables
+# Database (MySQL) endpoint server with credentials
+# Usage:
+#   - 'mysql://[database_user]:[password]@tcp([database_server_ip]:[database_server_port])/[database_name]''
+export K3S_DATASTORE_ENDPOINT='mysql://k3s:spindle-trace@tcp(10.175.134.133:3306)/k3s'
+
+# Install k3s on server 1
+# Usage:
+#   - 'curl -sfL https://get.k3s.io | sh -s - server --node-taint CriticalAddonsOnly=true:NoExecute --tls-san [load_balancer_ip]
+curl -sfL https://get.k3s.io | sh -s - server --node-taint CriticalAddonsOnly=true:NoExecute --tls-san 10.175.134.130
+
+# Install k3s on server 2
+# Obtain Kubernetes key from first server
+#   - sudo cat /var/lib/rancher/k3s/server/node-token
+# Same as first server, run the install command, but with the --token flag
+#   - 'curl -sfL https://get.k3s.io | sh -s - server --node-taint CriticalAddonsOnly=true:NoExecute --tls-san [load_balancer_ip]
+curl -sfL https://get.k3s.io | sh -s - server --node-taint CriticalAddonsOnly=true:NoExecute --tls-san 10.175.134.130 a;slfja;slgha;oifgjha;sodih a;rog
